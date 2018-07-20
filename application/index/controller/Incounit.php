e65a34bf59dd5647cddf7c9680ba29875e3bccd6
wqzyq <41100288@qq.com>
2018-07-20 18:09:29 +0800

    20180720xiugai

--- application/index/controller/Incounit.php
+++ application/index/controller/Incounit.php
@@ -15,7 +15,6 @@ class Incounit extends Controller
 
     public function customer()
     {
-        //echo 'customer';
         if (request()->isPost()) {
             $data = [
                 'UnitName' => input('UnitName'),
@@ -26,7 +25,7 @@ class Incounit extends Controller
                 'DutyParagraph' => input('DutyParagraph'),
                 'UnitCategory' => input('UnitCategory'),
                 'AffiliatedArea' => input('AffiliatedArea'),
-                'UnitAddres' => input('UnitAddres'),
+                'UnitAddress' => input('UnitAddress'),
                 'Contacts' => input('Contacts'),
                 'ContactNumber' => input('ContactNumber'),
                 'LegalPerson' => input('LegalPerson'),
@@ -40,7 +39,7 @@ class Incounit extends Controller
             $incounit->DutyParagraph = $data['DutyParagraph'];
             $incounit->UnitCategory = $data['UnitCategory'];
             $incounit->AffiliatedArea = $data['AffiliatedArea'];
-            $incounit->UnitAddres = $data['UnitAddres'];
+            $incounit->UnitAddress = $data['UnitAddress'];
             $incounit->Contacts = $data['Contacts'];
             $incounit->ContactNumber = $data['ContactNumber'];
             $incounit->LegalPerson = $data['LegalPerson'];
@@ -54,8 +53,58 @@ class Incounit extends Controller
 
     }
 
-    public function index1()
+    public function customerlist()
     {
-        echo 'index1';
+        $list = IncounitModel::all();
+        $this->assign('list', $list);
+        $this->assign('count', count($list));
+        return $this->fetch('Incounit/customerlist');
     }
+
+    public function customerdel()
+    {
+        $id = input('id');
+        if ($id > 0) {
+            if (db('incounit')->delete(input('id'))) {
+                $this->success('删除客户成功!', 'customerlist');
+            } else {
+                $this->error('删除客户失败!');
+            }
+        } else {
+            $this->error('初始化客户不能删除!');
+        }
+    }
+
+    public function cueditlist()
+    {
+        $id = input('id');
+        $list = db('incounit')->select($id);
+        $this->assign('list', $list);
+        return $this->fetch('Incounit/cueditlist');
+    }
+
+    public function cueditsave()
+    {
+        if(request()->isPost()) {
+            $id = input('id');
+            $data = [
+                'UnitName' => input('UnitName'),
+                'Bank' => input('Bank'),
+            ];
+            $incounit = IncounitModel::get($id);
+            $incounit->UnitName = $data['UnitName'];
+            $incounit->Bank = $data['Bank'];
+            if ($incounit->save()) {
+                return '用户[ ' . $incounit->UnitName . ' ]修改成功';
+            } else {
+                return $incounit->getError();
+            }
+        }else{
+            echo "你不是通过POST上传的字段,请先打开客户列表进行修改";
+        }
+
+
+    }
+
+
 }
